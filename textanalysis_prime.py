c05713118f7124569c864081a09dfca9fbea3eeb
Vinu P Tomy <vinuptomy@outlook.com>
2023-11-08 14:26:06 +0100

    Update textanalysis_prime.py

--- textanalysis_prime.py
+++ textanalysis_prime.py
@@ -1,59 +1,96 @@
-import feedparser as fp
-import urllib
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-import dautil as dl
-from nltk.corpus.reader import CategorizedPlaintextCorpusReader
-import os
+# import libraries
+import pandas as pd
+
+import nltk
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+from nltk.corpus import stopwords
+
+from nltk.tokenize import word_tokenize
+
+from nltk.stem import WordNetLemmatizer
 
 
-DRIVER = webdriver.PhantomJS()
-NAP_SECONDS = 10
-LOGGER = dl.log_api.conf_logger('corpus')
+# download nltk corpus (first time only)
+import nltk
 
-def store_txt(url, fname, title):
-    try:
-        DRIVER.get(url)
-
-        elems = WebDriverWait(DRIVER, NAP_SECONDS).until(
-            EC.presence_of_all_elements_located((By.XPATH, '//p'))
-        )
-
-        with open(fname, 'w') as txt_file:
-            txt_file.write(title + '\n\n')
-            lines = [e.text for e in elems]
-            txt_file.write(' \n'.join(lines))
-    except Exception:
-        LOGGER.error("Error processing HTML", exc_info=True)
+nltk.download('all')
 
 
-def fetch_news(dir):
-    base = 'http://newsrss.bbc.co.uk/rss/newsonline_uk_edition/{}/rss.xml'
 
-    for category in ['world', 'technology']:
-        rss = fp.parse(base.format(category))
 
-        for i, entry in enumerate(rss.entries):
-            fname = '{0}_bbc_{1}.txt'.format(i, category)
-            fname = os.path.join(dir, fname)
+# Load the amazon review dataset
 
-            if not dl.conf.file_exists(fname):
-                store_txt(entry.link, fname, entry.title)
+df = pd.read_csv('https://raw.githubusercontent.com/pycaret/pycaret/master/datasets/amazon.csv')
 
-if __name__ == "__main__":
-    dir = os.path.join(dl.data.get_data_dir(), 'bbc_news_corpus')
+df
 
-    if not os.path.exists(dir):
-        os.mkdir(dir)
+# create preprocess_text function
+def preprocess_text(text):
 
-    fetch_news(dir)
-    reader = CategorizedPlaintextCorpusReader(dir, r'.*bbc.*\.txt',
-                                              cat_pattern=r'.*bbc_(\w+)\.txt')
-    printer = dl.log_api.Printer(nelems=3)
-    printer.print('Categories', reader.categories())
-    printer.print('World fileids', reader.fileids(categories=['world']))
-    printer.print('Technology fileids',
-                  reader.fileids(categories=['technology']))
-          
+    # Tokenize the text
+
+    tokens = word_tokenize(text.lower())
+
+
+
+
+    # Remove stop words
+
+    filtered_tokens = [token for token in tokens if token not in stopwords.words('english')]
+
+
+
+
+    # Lemmatize the tokens
+
+    lemmatizer = WordNetLemmatizer()
+
+    lemmatized_tokens = [lemmatizer.lemmatize(token) for token in filtered_tokens]
+
+
+
+
+    # Join the tokens back into a string
+
+    processed_text = ' '.join(lemmatized_tokens)
+
+    return processed_text
+
+# apply the function df
+
+df['reviewText'] = df['reviewText'].apply(preprocess_text)
+df
+
+# initialize NLTK sentiment analyzer
+
+analyzer = SentimentIntensityAnalyzer()
+
+
+# create get_sentiment function
+
+def get_sentiment(text):
+
+    scores = analyzer.polarity_scores(text)
+
+    sentiment = 1 if scores['pos'] > 0 else 0
+
+    return sentiment
+
+
+
+
+# apply get_sentiment function
+
+df['sentiment'] = df['reviewText'].apply(get_sentiment)
+
+df
+
+from sklearn.metrics import confusion_matrix
+
+print(confusion_matrix(df['Positive'], df['sentiment']))
+
+from sklearn.metrics import classification_report
+
+print(classification_report(df['Positive'], df['sentiment']))
